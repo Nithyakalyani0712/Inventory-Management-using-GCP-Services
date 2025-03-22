2c567e09b62964f98a811946c3ab2096d19af675
Nithyakalyani0712 <nithyakalyanideena@gmail.com>
2025-03-22 23:07:33 +0530

    test

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ variable "credentials_file" {
 }
 
 provider "google" {
-  credentials = jsondecode(var.credentials_json)
+  credentials = file(var.credentials_file)
   project     = "possible-arbor-449017-k8"
   region      = "us-central1"
 }
