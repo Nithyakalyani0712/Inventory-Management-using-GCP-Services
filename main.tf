76048e3edd8a18466c41fc48b190b9a27de58298
Nithyakalyani0712 <nithyakalyanideena@gmail.com>
2025-03-22 21:21:54 +0530

    test

--- main.tf
+++ main.tf
@@ -1,8 +1,13 @@
-provider "google" {
-  credentials = jsondecode(var.credentials_json)
-  project     = var.project_id
-  region      = var.region
+variable "credentials_file" {
+  type = string
 }
+
+provider "google" {
+  credentials = file(var.credentials_file)
+  project     = "possible-arbor-449017-k8"
+  region      = "us-central1"
+}
+
 
 resource "google_storage_bucket" "bucket" {
   name          = var.bucket_name
